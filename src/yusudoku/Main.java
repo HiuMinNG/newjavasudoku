d88ddb7435e448129fe0ca9d90c763e827da6041
COMPUTER <COMPUTER@DESKTOP-QDQDI36>
2024-06-26 01:00:36 +0700

    First commit

--- src/yusudoku/Main.java
+++ src/yusudoku/Main.java
@@ -4,15 +4,9 @@ package yusudoku;
 import javax.swing.SwingUtilities;
 import yusudoku.gui.windows.WelcomeWindow;
 
-/**
- *
- * @author nathan
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new WelcomeWindow());
     }
